b6644e40e433d7fd4b0752145f2f0af79a7f8db7
Janne <bhi256@myy.haaga-helia.fi>
2025-11-08 17:07:24 +0200

    Added page for cities by specific region

--- harjoitustyo/src/main/java/com/example/harjoitustyo/domain/CityRepository.java
+++ harjoitustyo/src/main/java/com/example/harjoitustyo/domain/CityRepository.java
@@ -1,7 +1,11 @@
 package com.example.harjoitustyo.domain;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 public interface CityRepository extends CrudRepository<City, Long>{
+
+    List<City> findByRegion(Region region);
 
 }
---
+++ harjoitustyo/src/main/java/com/example/harjoitustyo/web/RegionController.java
@@ -0,0 +1,38 @@
+package com.example.harjoitustyo.web;
+
+import java.util.Optional;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.server.ResponseStatusException;
+
+import com.example.harjoitustyo.Exception.CustomNotFoundException;
+import com.example.harjoitustyo.domain.CityRepository;
+import com.example.harjoitustyo.domain.Region;
+import com.example.harjoitustyo.domain.RegionRepository;
+
+@Controller
+public class RegionController {
+
+    private RegionRepository rRepository;
+    private CityRepository cRepository;
+
+    public RegionController(RegionRepository rRepository, CityRepository cRepository) {
+        this.rRepository = rRepository;
+        this.cRepository = cRepository;
+    }
+
+    @GetMapping(value = { "/region/{id}" })
+    public String getRegion(@PathVariable("id") Long regionId, Model model) {
+        Region region = rRepository.findById(regionId)
+                .orElseThrow(() -> new CustomNotFoundException("Region by the id of " + regionId + " does not exist."));
+        model.addAttribute("region", region);
+        model.addAttribute("cities", cRepository.findByRegion(region));
+        return "region";
+
+    }
+
+}
