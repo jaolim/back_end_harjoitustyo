b8a7920c3ef0f3d4ab4fd6789bb60e8fb84c9632
Janne <bhi256@myy.haaga-helia.fi>
2025-11-08 18:29:12 +0200

    Location controller and page added

--- harjoitustyo/src/main/java/com/example/harjoitustyo/domain/CommentRepository.java
+++ harjoitustyo/src/main/java/com/example/harjoitustyo/domain/CommentRepository.java
@@ -1,7 +1,11 @@
 package com.example.harjoitustyo.domain;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 public interface CommentRepository extends CrudRepository<Comment, Long>{
+
+            List<Comment> findByLocation(Location location);
 
 }
---
+++ harjoitustyo/src/main/java/com/example/harjoitustyo/web/LocationController.java
@@ -0,0 +1,41 @@
+package com.example.harjoitustyo.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.example.harjoitustyo.Exception.CustomNotFoundException;
+import com.example.harjoitustyo.domain.City;
+import com.example.harjoitustyo.domain.Location;
+import com.example.harjoitustyo.domain.LocationRepository;
+import com.example.harjoitustyo.domain.Region;
+import com.example.harjoitustyo.domain.CommentRepository;
+
+@Controller
+public class LocationController {
+
+    private LocationRepository lRepository;
+    private CommentRepository coRepository;
+
+    public LocationController(LocationRepository lRepository, CommentRepository coRepository) {
+        this.lRepository = lRepository;
+        this.coRepository = coRepository;
+    }
+
+    @GetMapping(value = { "/location/{id}" })
+    public String getLocation(@PathVariable("id") Long locationId, Model model) {
+        Location location = lRepository.findById(locationId)
+                .orElseThrow(() -> new CustomNotFoundException("Location by the id of " + locationId + " does not exist."));
+        City city = location.getCity();
+        Region region = city.getRegion();
+        model.addAttribute(location);
+        model.addAttribute("city", city);
+        model.addAttribute("region", region);
+        model.addAttribute("comments", coRepository.findByLocation(location));
+        return "location";
+
+    }
+
+}
+
