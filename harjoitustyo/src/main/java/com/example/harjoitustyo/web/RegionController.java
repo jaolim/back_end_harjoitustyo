8d6110b5d6f216ec7e186d5afb879bdf41b70a44
Janne <bhi256@myy.haaga-helia.fi>
2025-11-10 17:35:30 +0200

    Added edit page and controller for regions

--- harjoitustyo/src/main/java/com/example/harjoitustyo/web/RegionController.java
+++ harjoitustyo/src/main/java/com/example/harjoitustyo/web/RegionController.java
@@ -1,5 +1,7 @@
 package com.example.harjoitustyo.web;
 
+import java.util.Optional;
+
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -15,7 +17,6 @@ import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
 
 @Controller
 public class RegionController {
@@ -58,13 +59,38 @@ public class RegionController {
     @PostMapping("/region/save")
     public String saveRegion(Region region, HttpServletRequest request, RedirectAttributes redirectAttributes) {
         String referer = request.getHeader("Referer");
-        if(rRepository.existsByName(region.getName())){
-            redirectAttributes.addFlashAttribute("errorMessage", "Region by the name of " + region.getName() + " already exists.");
-            return "redirect:" + referer;
-
+        if (rRepository.existsByName(region.getName())) {
+            if (region.getRegionId() != null) {
+                Region isSame = rRepository.findByName(region.getName());
+                if (isSame.getRegionId() != region.getRegionId()) {
+                    redirectAttributes.addFlashAttribute("errorMessage",
+                            "Region by the name of " + region.getName() + " already exists.");
+                    return "redirect:" + referer;
+                }
+            }
+            if (region.getRegionId() == null) {
+                redirectAttributes.addFlashAttribute("errorMessage",
+                        "Region by the name of " + region.getName() + " already exists.");
+                return "redirect:" + referer;
+            }
         }
         rRepository.save(region);
         return "redirect:..";
+    }
+
+    @PreAuthorize("hasAuthority('ADMIN')")
+    @GetMapping("/region/edit/{id}")
+    public String editRegion(@PathVariable("id") Long regionId, Model model, HttpServletRequest request,
+            RedirectAttributes redirectAttributes) {
+        String referer = request.getHeader("Referer");
+        Optional<Region> region = rRepository.findById(regionId);
+        if (!region.isPresent()) {
+            redirectAttributes.addFlashAttribute("errorMessage",
+                    "Region by the ID of " + regionId + " does not exist.");
+            return "redirect:" + referer;
+        }
+        model.addAttribute("region", region);
+        return "regionEdit";
     }
 
 }
